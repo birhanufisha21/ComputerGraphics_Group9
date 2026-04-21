137b019ad98e4837d50cd9285b048287fc89c20d
atinaw <swetmar503@gmail.com>
2026-04-21 12:22:35 +0300

    Implement drawVertexWaved and drawMaasaiShapeRaw functions
    Added functions to draw waved vertices and a Maasai shape.

--- main.cpp
+++ main.cpp
@@ -22,6 +22,29 @@ void setKWhite() { glColor3f(1.0f, 1.0f, 1.0f); }
 void setTikTokMagenta() { glColor3f(0.996f, 0.173f, 0.333f); }
 void setTikTokCyan()    { glColor3f(0.145f, 0.957f, 0.933f); }
 void setTTBlack()       { glColor3f(0.0f, 0.0f, 0.0f); }
+void drawVertexWaved(float x, float y) {
+    float multiplier = (x + 200.0f) / 400.0f;
+    if (multiplier < 0.0f) multiplier = 0.0f;
+    if (multiplier > 1.0f) multiplier = 1.0f;
+
+    float yOffset = multiplier * WAVE_AMP * sin(WAVE_FREQ * x + flagTime);
+    glVertex2f(x, y + yOffset);
+}
+
+void drawMaasaiShapeRaw(float maxWidth, float height, float P) {
+    glBegin(GL_POLYGON);
+    for (int i = 0; i <= 100; i++) {
+        float t = -1.0f + 2.0f * i / 100.0f;
+        float width = maxWidth * pow(1.0f - pow(fabs(t), P), 1.0f / P);
+        drawVertexWaved(width, t * height);
+    }
+    for (int i = 100; i >= 0; i--) {
+        float t = -1.0f + 2.0f * i / 100.0f;
+        float width = -maxWidth * pow(1.0f - pow(fabs(t), P), 1.0f / P);
+        drawVertexWaved(width, t * height);
+    }
+    glEnd();
+}
 // Placeholder functions for members to fill
 void drawFlagPole() {}
 void drawMaasaiShapeRaw(float mw, float h, float p) {}
